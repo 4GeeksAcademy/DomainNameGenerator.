216116231000789e53dd1cbe351fd24807ff099a
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-10-23 05:26:09 +0000

    logic ok

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let button = document.querySelector("#domain");
+let container = document.querySelector("#container");
+let NamesDom = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+button.addEventListener("click", function() {
+  let randomDomain = NamesDom[Math.floor(Math.random() * NamesDom.length)];
+
+  let newH1 = document.createElement("h1");
+  newH1.innerText = randomDomain;
+
+  container.appendChild(newH1);
+});
+
+let articles = ["the", "our", "an", "Some"];
+let adjs = ["awesome", "cool", "intriguing", "thrilling", "funny"];
+let nouns = ["website", "blog", "webapp", "spot", "hub"];
+let tlds = [".com", ".es", ".uy", ".br", ".ar", ".mx", ".org"];
+
+function domainNames() {
+  for (let article of articles) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          NamesDom.push(`${article}${adj}${noun}${tld}`);
+        }
+      }
+    }
+  }
+}
+
+domainNames();
